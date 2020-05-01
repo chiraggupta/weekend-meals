deef3463c474655c9442f72167c062a1af3cfcfa
Chirag Gupta <echirag@gmail.com>
2020-05-01 15:05:26 +0100

    Update getNextOrderer to take list of orders as input

--- src/utils/getNextOrderer.js
+++ src/utils/getNextOrderer.js
@@ -1,7 +1,12 @@
 const ROTA = ['Anya', 'Babina', 'Yoshi', 'Chirag'];
 
-export default (lastOrderer) => {
-  const lastOrdererIndex = ROTA.indexOf(lastOrderer);
+export default (orders) => {
+  if (!orders || orders.length === 0) {
+    return ROTA[0];
+  }
+
+  const latestOrderer = orders[0].orderer;
+  const lastOrdererIndex = ROTA.indexOf(latestOrderer);
   const nextOrdererIndex = (lastOrdererIndex + 1) % ROTA.length;
   return ROTA[nextOrdererIndex];
 };
