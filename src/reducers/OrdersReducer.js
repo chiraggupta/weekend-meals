c245e853fbd19e0040b24eed82c136db4143e032
Chirag Gupta <echirag@gmail.com>
2020-05-01 15:05:49 +0100

    Create OrdersReducer

---
+++ src/reducers/OrdersReducer.js
@@ -0,0 +1,16 @@
+import getNextOrderer from '../utils/getNextOrderer';
+
+export const initialState = { orders: [], nextOrderer: getNextOrderer() };
+
+const OrdersReducer = (state, action) => {
+  if (action.type === 'UPDATE') {
+    return {
+      orders: action.orders,
+      nextOrderer: getNextOrderer(action.orders),
+    };
+  }
+
+  return state;
+};
+
+export default OrdersReducer;
