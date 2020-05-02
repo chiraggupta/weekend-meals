d1e1160e4cfdcb543554645c3ee425a3fc7cd47f
Chirag Gupta <echirag@gmail.com>
2020-05-02 09:36:40 +0100

    Fix uuid

--- src/components/OrderList.js
+++ src/components/OrderList.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import uuid from 'uuid/v4';
+import { v4 as uuid } from 'uuid';
 
 import ordersApi from '../services/ordersApi';
 import Order from './Order';
