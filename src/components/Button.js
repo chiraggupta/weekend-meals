7127d9867c999a3e22d67a1bc644ae5b873b8b21
Chirag Gupta <echirag@gmail.com>
2020-05-09 17:42:49 +0100

    Use CSS in JS again

---
+++ src/components/Button.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default ({ children }) => (
+  <button
+    style={{
+      padding: '0.8rem 1.8rem',
+      width: '100%',
+      border: 'none',
+      borderRadius: '0.2rem',
+      textDecoration: 'none',
+      fontFamily: 'inherit',
+      fontSize: '1.2rem',
+      color: '#fff',
+      backgroundColor: '#be4b77',
+      boxShadow:
+        'inset 0 -0.5rem 1rem -0.5rem rgba(0, 0, 0, 0.3), inset 0 0.5rem 2rem -0.5rem rgba(255, 255, 255, 0.2), inset 0 0 0rem 0.1rem rgba(255, 255, 255, 0.1)',
+    }}
+  >
+    {children}
+  </button>
+);
--- src/components/Button/Button.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import './Button.css';
-
-export default ({ children }) => (
-  <button className="Button-main">{children}</button>
-);
