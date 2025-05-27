d0864ee894b493f09686f4a51f4f99d817a661de
Chirag Gupta <echirag@gmail.com>
2025-05-27 23:04:19 +0100

    Improved logging

--- functions/orders/setupFirestore.js
+++ functions/orders/setupFirestore.js
@@ -2,26 +2,21 @@ const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
 const raw = process.env.FIRESTORE_SA_BASE64;
-console.log('Raw length:', raw?.length || 'undefined');
-console.log('Raw preview:', raw?.slice(0, 30)); // first 30 chars
 
 try {
   const decoded = Buffer.from(raw, 'base64').toString();
-  console.log('Decoded preview:', decoded.slice(0, 30)); // should look like JSON
   const credentials = JSON.parse(decoded);
-  console.log('✅ JSON parsed:', credentials.project_id);
 
   initializeApp({
     credential: cert(credentials)
   });
 
-} catch (err) {
-  console.error('❌ Error decoding or parsing Firestore credentials:', err.message);
+} catch (error) {
+  console.error('Error (setupFirestore):', error);
+  return {
+    statusCode: 500,
+    body: 'Error setting up Firestore',
+  };
 }
-
-// const credentials = JSON.parse(
-//   Buffer.from(process.env.FIRESTORE_SA_BASE64, 'base64').toString()
-// );
-
 
 exports.db = getFirestore();
