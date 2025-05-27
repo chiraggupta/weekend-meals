aa17543c4c20ca0ca61d9125e27396be639f0dc8
Chirag Gupta <echirag@gmail.com>
2025-05-27 22:09:06 +0100

    Remove typo

--- functions/orders/setupFirestore.js
+++ functions/orders/setupFirestore.js
@@ -1,11 +1,9 @@
 const { initializeApp, cert } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
 
-const saBase64 = JSON.parse(process.env.FIRESTORE_SA_BASE64);
 const credentials = JSON.parse(
-  Buffer.from(saBase64, 'base64').toString()
+  Buffer.from(process.env.FIRESTORE_SA_BASE64, 'base64').toString()
 );
-
 
 initializeApp({
   credential: cert(credentials)
